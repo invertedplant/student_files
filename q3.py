ab4e91ffe8d47806d1f9d5cecae50a0b8b027434
invertedplant <invertedplant@gmail.com>
2021-12-10 00:58:22 +0800

    Q3 and Q4?
    I hope this works lol

--- q3.py
+++ q3.py
@@ -1,11 +1,47 @@
+import ast
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
 # you may add more import if you need to
 
 
 # don't change this line
+
 hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
+sc = spark.SparkContext
+
+
+def foreach(record):
+    record_l = ast.literal_eval(record[1])
+    idx = record[0]
+    reviews = record_l[0]
+    dates = record_l[1]
+    out = []
+    for i in range(len(reviews)):
+        out.append((idx, reviews[i], dates[i]))
+
+    return out
+
+
+df = spark.read.option("header", True).csv("hdfs://%s:9000/assignment2/part1/input/" % hdfs_nn)
+r = df.select("ID_TA", "Reviews")
+rdd = r.rdd.map(lambda ls: [ls[0], ls[1]])
+output = rdd.map(foreach)
+out_data = list(output.collect())
+
+d = []
+for x in out_data:
+    for y in x:
+        d.append(y)
+
+df_new = spark.createDataFrame(d, ["ID_TA", "review", "date"]).show()
+
+df_new.write.option("header", True).csv("hdfs://%s:9000/assignment2/output/question3" % hdfs_nn)
+# out_df.saveAsTextFile("hdfs://%s:9000/assignment2/output/question1" % (hdfs_nn))
+
+
+spark.stop()
--- q4.py
+++ q4.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
 
 # you may add more import if you need to
 
@@ -9,3 +10,9 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 4").getOrCreate()
 # YOUR CODE GOES BELOW
+sc = spark.SparkContext
+
+df = spark.read.option("header", True).csv("hdfs://%s:9000/assignment2/part1/input/" % hdfs_nn)
+df1 = df.select("City", explode("Cuisine")).groupby("Cuisine").count().show()
+
+df1.write.option("header", True).csv("hdfs://%s:9000/assignment2/output/question4" % hdfs_nn)
